d2fd254dd7657cc78dc8e755fc3c869cb8d1b641
Matteo Raso <matteo_luigi_raso@protonmail.com>
2022-09-24 17:23:40 -0400

    FIX: Added missing argument

--- markov_chain/briscola_game.py
+++ markov_chain/briscola_game.py
@@ -17,7 +17,7 @@ def briscola_game(player_1, player_2):
     card_1 = player_1.play_card(player_2)
     card_2 = player_2.play_card(player_1)
     del playing_deck[0:6]
-    player_1, player_2 = better_card(card_1, card_2, player_1, player_2)
+    player_1, player_2 = better_card(card_1, card_2, player_1, player_2, briscola)
 
     while len(playing_deck) > 0:
         #In Briscola, whoever has the best card draws and plays first.
@@ -26,14 +26,14 @@ def briscola_game(player_1, player_2):
             playing_deck.remove(0)
             player_2.hand.append(playing_deck[0])
             playing_deck.remove(0)
-            player_1, player_2 = better_card(card_1, card_2, player_1, player_2)
+            player_1, player_2 = better_card(card_1, card_2, player_1, player_2, briscola)
 
         else:
             player_2.hand.append(playing_deck[0])
             playing_deck.remove(0)
             player_1.hand.append(playing_deck[0])
             playing_deck.remove(0)
-            player_2, player_1 = better_card(card_1, card_2, player_2, player_1)
+            player_2, player_1 = better_card(card_1, card_2, player_2, player_1, briscola)
 
     if player_1.points >= player_2.points:
         player_1.wins += 1
