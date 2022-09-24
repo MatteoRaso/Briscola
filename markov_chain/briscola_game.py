723076f186a6b3b5267ef1c4fa0fa669f76bb49d
Matteo Raso <matteo_luigi_raso@protonmail.com>
2022-09-24 02:25:33 -0400

    FIX: Called function instead of using it as argument
    What I was doing was passing the deck function itself as
    an argument for np.random.shuffle. Now, I call the function
    and assign the output to playing_deck.

--- markov_chain/briscola_game.py
+++ markov_chain/briscola_game.py
@@ -8,30 +8,31 @@ import numpy as np
 def briscola_game(player_1, player_2):
     index = np.random.randint(0, 4)
     briscola = ['B', 'C', 'D', 'S'][index]
-    np.random.shuffle(deck)
-    player_1.hand = deck[0:3]
-    player_2.hand = deck[3:6]
+    playing_deck = deck(briscola)
+    np.random.shuffle(playing_deck)
+    player_1.hand = playing_deck[0:3]
+    player_2.hand = playing_deck[3:6]
     player_1.points = 0
     player_2.points = 0
     card_1 = player_1.play_card(player_2)
     card_2 = player_2.play_card(player_1)
-
+    del playing_deck[0:6]
     player_1, player_2 = better_card(card_1, card_2, player_1, player_2)
 
-    while len(deck) > 0:
+    while len(playing_deck) > 0:
         #In Briscola, whoever has the best card draws and plays first.
         if player_1.best:
-            player_1.hand.append(deck[0])
-            deck.remove(0)
-            player_2.hand.append(deck[0])
-            deck.remove(0)
+            player_1.hand.append(playing_deck[0])
+            playing_deck.remove(0)
+            player_2.hand.append(playing_deck[0])
+            playing_deck.remove(0)
             player_1, player_2 = better_card(card_1, card_2, player_1, player_2)
 
         else:
-            player_2.hand.append(deck[0])
-            deck.remove(0)
-            player_1.hand.append(deck[0])
-            deck.remove(0)
+            player_2.hand.append(playing_deck[0])
+            playing_deck.remove(0)
+            player_1.hand.append(playing_deck[0])
+            playing_deck.remove(0)
             player_2, player_1 = better_card(card_1, card_2, player_2, player_1)
 
     if player_1.points >= player_2.points:
