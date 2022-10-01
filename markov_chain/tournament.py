81065844d95fc5ded78388a2093b7dca2ad51b27
Matteo Raso <matteo_luigi_raso@protonmail.com>
2022-10-01 02:58:33 -0400

    Optimized tournament.py
    To have more accurate results, I looked at 99
    different gamma values instead of 19. Since some
    of the results were close together, I also increased
    the number of games that had to be played in the tournament.

--- markov_chain/tournament.py
+++ markov_chain/tournament.py
@@ -4,18 +4,18 @@ from briscola_game import *
 
 players = []
 
-for i in range(1, 20):
+for i in range(1, 100):
     players.append(player())
     players[i - 1]._init_()
     players[i - 1].initialize_policy_and_value()
-    players[i - 1].gamma = 0.05 * i
+    players[i - 1].gamma = 0.01 * i
     players[i - 1].training_iterations = 300000
     players[i - 1].train()
 
 for i in range(0, len(players)):
     for j in range(i, len(players)):
         if i != j:
-            for k in range(0, 1000):
+            for k in range(0, 10000):
                 briscola_game(players[i], players[j])
 
 players.sort(key = lambda x: x.wins, reverse = True)
